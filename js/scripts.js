befcaedac6b70231ededb74cd4ad9a6f7255ca9a
diahanara <diahanarahma@gmail.com>
2025-04-29 17:25:07 +0700

    coba

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,3 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
-const images = document.querySelector('.carousel-images');
-const dots = document.querySelectorAll('.dot');
-let currentIndex = 0;
-
-function showSlide(index) {
-  images.style.transform = `translateX(-${index * 400}px)`;
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[index].classList.add('active');
-}
-
-document.querySelector('.prev').onclick = () => {
-  currentIndex = (currentIndex === 0) ? 2 : currentIndex - 1;
-  showSlide(currentIndex);
-};
-
-document.querySelector('.next').onclick = () => {
-  currentIndex = (currentIndex === 2) ? 0 : currentIndex + 1;
-  showSlide(currentIndex);
-};
-
-dots.forEach((dot, index) => {
-  dot.onclick = () => {
-    currentIndex = index;
-    showSlide(index);
-  };
-});
