c5e4c61edc32affcf5360d11c1a6a64945eb80d8
diahanara <diahanarahma@gmail.com>
2025-04-29 17:39:22 +0700

    instagram

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+const carousel = document.querySelector('.carousel');
+const images = document.querySelectorAll('.carousel img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function updateCarousel() {
+  const width = images[0].clientWidth;
+  carousel.style.transform = `translateX(-${index * width}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
